fc02c2660001403ecadece9de3fdf83fbc4565fa
navya <kakumaninavyasri@gmail.com>
2022-08-15 16:32:31 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React,{useEffect, useState} from 'react';
+import Products from './Products';
+import OneSignal from 'react-onesignal';
+
+const App = () => {
+  const [search,setSearch]=useState('');
+  const [data,setData]=useState([]);
+  const YOUR_APP_ID ="57b61f27";
+  const YOUR_APP_KEY ="abe20662438de76e408e64547e5c5c0b";
+  useEffect(()=>{
+    OneSignal.init({appId:'a3fc4253-ab37-486e-b91d-415a85c5c161'})
+      })
+  const submithandler=(e)=>{
+    e.preventDefault();
+    console.log(search);
+    fetch(`https://api.edamam.com/search?q=${search}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=30&calories=591-722`).then(
+    response=>response.json()
+    ).then(
+      data=>setData(data.hits)
+    )
+    
+  }
+  return (
+    <div>
+      <center>
+      <h4>Food Recipie App</h4> <br/>
+      <form onSubmit={submithandler}>
+        <input type="text" value={search} onChange={(e)=>setSearch(e.target.value)} /> <br/> <br/>
+        <input type="submit" value="Search" className='btn btn-primary'/> 
+      </form> <br/>
+      {data.length >=1 ?<Products data={data}/>:null}
+      
+      
+      </center>
+    </div>
+  );
+};
+
+export default App;
